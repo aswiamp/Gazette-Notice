551ba3d79c2db2fb2beec56459191be86574f5af
aswiamp <amp29148@gmail.com>
2023-04-20 15:14:02 +0530

    implement next button

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const axios=require('axios');
-const express=require('express')
+const axios = require('axios');
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/notice', async (req, res) => {
-  try {
-    //fetch data from a json api
-    const notice = await axios.get("https://www.thegazette.co.uk/all-notices/notice/data.json");
-    //const noticeData = await not.json();
-    res.json(notice.data);//send it back as a json response 
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
+    try {
+        //fetch data from a json api
+        const notice = await axios.get(
+            'https://www.thegazette.co.uk/all-notices/notice/data.json'
+        );
+        //const noticeData = await not.json();
+        res.json(notice.data); //send it back as a json response
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error');
+    }
 });
 
 app.use((req, res) => {
-  res.status(404).send('Not found');
+    res.status(404).send('Not found');
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
